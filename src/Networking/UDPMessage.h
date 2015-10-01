61ac9d3c7e8db55ecc6f11a268602f482e23777c
Weikie <weikie_yeh@hotmail.com>
2015-10-01 16:57:29 +0200

    +Added UDPMessage and base64 classes

---
+++ src/Networking/UDPMessage.h
@@ -0,0 +1,19 @@
+#ifndef udpmessage_h
+#define udpmessage_h
+#include <codecvt>
+#include <SDL2Net/SDL_net.h>
+#include "Patterns/singleton.h"
+
+class UDPMessage// : Singleton<UDPMessage>
+{
+	public:
+		UDPMessage(IPaddress ip);
+		void SendUnreliableMessage(Uint8 data);
+		static IPaddress StringToIP(std::string stringIP, int port);
+		bool ReceiveMessage(Uint8& outValue);
+	private:
+		UDPsocket socket;
+		IPaddress ip;
+};
+
+#endif /* udpmessage_h */

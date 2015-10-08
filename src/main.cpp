1750840de27e703787c78ccf7b79b7741c285250
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-10-08 14:59:47 +0200

    *Clean main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,9 @@
-// ImGui - standalone example application for SDL2 + OpenGL
-
-#define GLM_FORCE_RADIANS
-//default platform defines:
-//#ifdef _WIN32
-//#elif __APPLE__
-//#elif __linux__
-
-#include "standardIncludes.h"
-
-#include <stdio.h>
-#include <SDL2/SDL.h>
 #include "content.h"
-
 #include "Engine.h"
-
 
 int main(int argc, char** a)
 {
+    
 	Engine engine;
 	engine.InitSDL();
 

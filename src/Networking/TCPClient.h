fbac9c88838731c5862c91521b48c368e294d15d
Weikie <weikie_yeh@hotmail.com>
2015-10-08 14:59:31 +0200

    +Added TCPClient and TCPServer

---
+++ src/Networking/TCPClient.h
@@ -0,0 +1,14 @@
+#include <SDL2Net/SDL_net.h>
+
+class TCPClient
+{
+	public:
+		TCPClient(char* hostName, unsigned int port);
+		~TCPClient();
+		void SendReliableMessage(void* data, int length);
+		void ReceiveMessage();
+	private:
+		IPaddress ip;
+		TCPsocket socket;
+		const unsigned int MAX_MESSAGE_LENGTH = 1024;
+};
---
+++ src/Networking/TCPServer.h
@@ -0,0 +1,17 @@
+#include <SDL2Net/SDL_net.h>
+#include <vector>
+
+class TCPServer
+{
+	public:
+		TCPServer(unsigned int port);
+		~TCPServer();
+		void AcceptConnections();
+		void SendReliableMessage(void* data);
+		void ReceiveMessage();
+	private:
+		IPaddress ip;
+		TCPsocket serverSocket;
+		const unsigned int MAX_MESSAGE_LENGTH = 1024;
+		std::vector<TCPsocket> clients;
+};

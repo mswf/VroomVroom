bfd230231273af47fa80a01d6ef40149bc77a0d6
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-10-09 10:38:33 +0200

    *Added header guards and reformated some code

--- src/entity_system.h
+++ src/entity_system.h
@@ -1,18 +1,23 @@
+#ifndef entity_system_h
+#define entity_system_h
+
 #include <map>
 #include <vector>
-#include "component.h"
+//#include "component.h"
+
 typedef int FamilyId;
+
 struct EntitySystem;
 
 struct Entity
 {
-	static EntitySystem *entitySystem;
-	Entity();
-	template<typename Type> Type *getAs();
-	std::map<FamilyId, Component*> mComponents;
+	static EntitySystem* entitySystem;
+    Entity(){}
+	template<typename T> T* getAs();
+	std::map<FamilyId, Component* > mComponents;
 };
 
-EntitySystem *Entity::entitySystem = 0;
+EntitySystem* Entity::entitySystem = 0;
 
 struct EntitySystem
 {
@@ -20,32 +25,36 @@ struct EntitySystem
 		{
 			Entity::entitySystem = this;
 		}
-		template<typename T> T *getComponent(Entity *e)
+    
+		template<typename T>
+        T* getComponent( Entity* e )
 		{
 			return (T*)e->mComponents[T::familyId];
 		}
-		template<typename T> void getEntities(std::vector<Entity*> &result)
+    
+		template<typename T>
+        void getEntities( std::vector< Entity* > &result )
 		{
-			auto iterPair = mComponentStore.equal_range(T::familyId);
-			for (auto iter = iterPair.first; iter != iterPair.second; ++iter)
+			auto iterPair = mComponentStore.equal_range( T::familyId );
+			for ( auto iter = iterPair.first; iter != iterPair.second; ++iter )
 			{
-				result.push_back(iter->second);
+				result.push_back( iter->second );
 			}
 		}
-		template<typename T> void addComponent(Entity *e, T* comp)
+		
+        template<typename T>
+        void addComponent( Entity* e, T* comp )
 		{
-			mComponentStore.insert(std::pair<FamilyId, Entity*>(T::familyId, e));
-			e->mComponents.insert(std::pair<FamilyId, Component*>(T::familyId, comp));
+			mComponentStore.insert( std::pair< FamilyId, Entity* >( T::familyId, e ) );
+			e->mComponents.insert( std::pair< FamilyId, Component* >( T::familyId, comp ) );
 		}
 	protected:
-		std::multimap<FamilyId, Entity*> mComponentStore;
+		std::multimap< FamilyId, Entity* > mComponentStore;
 };
 
-inline Entity::Entity()
+template<typename T> T* Entity::getAs()
 {
+	return entitySystem->getComponent<T>( this );
 }
 
-template<typename Type> Type *Entity::getAs()
-{
-	return entitySystem->getComponent<Type>(this);
-}
+#endif /* entity_system_h */

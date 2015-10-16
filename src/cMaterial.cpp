c577bbe0e6e98df7797402df132ec8c7b736e9d7
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-10-16 19:36:34 +0200

    *Aesthetic changes

--- src/cMaterial.cpp
+++ src/cMaterial.cpp
@@ -1,9 +1,1 @@
-//
-//  cMaterial.cpp
-//  VroomVroom
-//
-//  Created by Valentinas Rimeika on 14/10/15.
-//  Copyright © 2015 Valentinas Rimeika. All rights reserved.
-//
-
-#include "cMaterial.hpp"
+#include "cMaterial.h"

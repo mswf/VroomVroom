f8ca97a6e1eb0f155a4d61b5b890c178cacc8137
Weikie Yeh <weikie_yeh@hotmail.com>
2015-10-20 13:52:04 +0200

    +Added base for receiving messages

--- src/Networking/NetMessage.cpp
+++ src/Networking/NetMessage.cpp
@@ -1,4 +1,6 @@
 #include "NetMessage.h"
+#include "../glm/glm.hpp"
+#include "guiddef.h"
 
 
 NetMessage::NetMessage():
@@ -9,4 +11,48 @@ NetMessage::NetMessage():
 
 NetMessage::~NetMessage()
 {
+}
+
+NetworkData NetMessage::ReceiveMessage()
+{
+	std::vector<NetworkData> messageList = client->ReceiveMessage();
+	for (std::vector<NetworkData>::iterator it = messageList.begin(); it != messageList.end(); ++it)
+	{
+		NetMessageType messageType;
+		int readIndex = 0;
+		HelperFunctions::ReadFromBuffer(it->data, readIndex, messageType);
+		switch (messageType)
+		{
+			case NetMessageType::SyncPosition:
+			{
+				_GUID* id;
+				//_GUID* id2(id);
+
+				glm::vec3 pos(0, 0, 0);
+				float time;
+				HelperFunctions::ReadFromBuffer(it->data, readIndex, id);
+				HelperFunctions::ReadFromBuffer(it->data, readIndex, pos);
+				HelperFunctions::ReadFromBuffer(it->data, readIndex, time);
+				break;
+			}
+			case NetMessageType::SyncRotation:
+			{
+				break;
+			}
+			case NetMessageType::SyncVelocity:
+			{
+				break;
+			}
+			case NetMessageType::SyncNpc:
+			{
+				break;
+			}
+			default:
+			{
+				assert(false);
+				break;
+			}
+		}
+	}
+	return messageList[0];
 }
--- src/Networking/NetMessage.h
+++ src/Networking/NetMessage.h
@@ -1,10 +1,13 @@
 #include "TCPClient.h"
-#include <Utilities/helperFunctions.h>
+#include "../Utilities/helperFunctions.h"
+#include "../standardIncludes.h"
 
-enum class NetMessageType
+enum class NetMessageType : short
 {
-	SyncPosition = 0, //[enityID,pos,time]
-
+	SyncPosition = 0,	//[entityID,(vec3)pos,(float)time]
+	SyncRotation,		//[entityID,(quat)rot,(float)time]
+	SyncVelocity,		//[entityID,(vec3)vel,(float)time]
+	SyncNpc
 };
 
 class NetMessage
@@ -14,17 +17,21 @@ class NetMessage
 		~NetMessage();
 		template <typename T>
 		void SendMessage(NetMessageType messageType, const T& data);
+		NetworkData ReceiveMessage();
 	private:
 		TCPClient* client;
+		const short NET_MESSAGE_TYPE_SIZE = 2;
 };
 
 template <typename T>
-void NetMessage::SendMessage(NetMessageType messageType, const T& data)
+void NetMessage::SendMessage(const NetMessageType messageType, const T& data)
 {
 	int typeSize = sizeof(T);
-	int messageTypeSize = sizeof(NetMessageType);
-	char* buffer = new char[typeSize + messageTypeSize];
+	assert(NET_MESSAGE_TYPE_SIZE == sizeof(NetMessageType));
+	char* buffer = new char[typeSize + NET_MESSAGE_TYPE_SIZE];
 	HelperFunctions::InsertIntoBuffer(buffer, 0, messageType);
-	HelperFunctions::InsertIntoBuffer(buffer, messageTypeSize, data);
-	client->SendMessage(buffer, typeSize + messageTypeSize);
+	HelperFunctions::InsertIntoBuffer(buffer, NET_MESSAGE_TYPE_SIZE, data);
+	client->SendMessage(buffer, typeSize + NET_MESSAGE_TYPE_SIZE);
+	delete buffer;
 }
+

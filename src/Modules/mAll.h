cda27ab793154993806ed4f0cd8787045667adae
Robin Zaagsma <zaagsma@Robins-MacBook-Pro.local>
2015-10-27 11:48:21 +0100

    +added mAll.h to Modules

---
+++ src/Modules/mAll.h
@@ -0,0 +1,15 @@
+//
+//  mAll.h
+//  VroomVroom
+//
+//  Created by Robin Zaagsma on 27/10/15.
+//  Copyright © 2015 Valentinas Rimeika. All rights reserved.
+//
+
+#ifndef mAll_h
+#define mAll_h
+
+#include "modules/mInput.h"
+#include "modules/mEngine.h"
+
+#endif /* mAll_h */

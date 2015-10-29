d72bd20ec9dece2d45948d76aa1ed4f9e6ab77df
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-10-29 17:23:18 +0100

    * Added traslation, rotation, scale functionality parent, children references

--- src/cTransform.cpp
+++ src/cTransform.cpp
@@ -2,7 +2,144 @@
 
 const int CTransform::familyId = (int)ComponentTypes::TRANSFORM;
 
-CTransform::CTransform()
+CTransform::CTransform( CTransform* parent )
+:	parent( parent ),
+	transform( glm::mat4(1.0) )
 {
-    
+}
+
+CTransform::~CTransform()
+{
+	
+}
+
+CTransform* CTransform::GetParent()
+{
+	return parent;
+}
+
+void CTransform::SetParent( CTransform* newParent )
+{
+	parent = newParent;
+}
+
+std::vector< CTransform* > CTransform::GetChildren()
+{
+	return children;
+}
+
+glm::vec3 CTransform::GetPosition() const
+{
+	return glm::vec3( transform[3] );
+}
+
+glm::vec3 CTransform::GetScale() const
+{
+	return glm::vec3( transform[0][3], transform[1][3], transform[2][3] );
+}
+
+void CTransform::SetPosition( glm::vec3 position )
+{
+	transform[3][0] = position.x;
+	transform[3][1] = position.y;
+	transform[3][2] = position.z;
+}
+
+void CTransform::SetScale( glm::vec3 scale )
+{
+	transform[0][3] = scale.x;
+	transform[1][3] = scale.y;
+	transform[2][3] = scale.z;
+}
+
+glm::vec3 CTransform::GetRotation() const
+{
+	return glm::eulerAngles( glm::quat_cast(transform) );
+}
+
+float CTransform::GetPitch() const
+{
+	return glm::pitch( glm::quat_cast(transform) );
+}
+
+float CTransform::GetYaw() const
+{
+	return glm::yaw( glm::quat_cast(transform) );
+}
+
+float CTransform::GetRoll() const
+{
+	return glm::roll( glm::quat_cast(transform) );
+}
+
+void CTransform::SetRotation( glm::vec3 rotation )
+{
+	glm::mat4 rot(1.0f);
+	rot = glm::rotate(rot, glm::radians( rotation.x ), glm::vec3( 1.0f, 0.0f, 0.0f ) );
+	rot = glm::rotate(rot, glm::radians( rotation.y ), glm::vec3( 0.0f, 1.0f, 0.0f ) );
+	rot = glm::rotate(rot, glm::radians( rotation.z ), glm::vec3( 0.0f, 0.0f, 1.0f ) );
+	SetRotationMatrix(rot);
+}
+
+void CTransform::SetRotationMatrix(glm::mat4 rot)
+{
+	transform[0][0] = rot[0][0];	transform[0][1] = rot[0][1];	transform[0][2] = rot[0][2];
+	transform[1][0] = rot[1][0];	transform[1][1] = rot[1][1];	transform[1][2] = rot[1][2];
+	transform[2][0] = rot[2][0];	transform[2][1] = rot[2][1];	transform[2][2] = rot[2][2];
+}
+
+void CTransform::SetPitch( float angle )
+{
+	glm::mat4 rot(1.0f);
+	rot = glm::rotate(rot, glm::radians( angle ), glm::vec3( 1.0f, 0.0f, 0.0f ) );
+	SetRotationMatrix(rot);
+}
+
+void CTransform::SetYaw( float angle )
+{
+	glm::mat4 rot(1.0f);
+	rot = glm::rotate(rot, glm::radians( angle ), glm::vec3( 0.0f, 1.0f, 0.0f ) );
+	SetRotationMatrix(rot);
+}
+
+void CTransform::SetRoll( float angle )
+{
+	glm::mat4 rot(1.0f);
+	rot = glm::rotate(rot, glm::radians( angle ), glm::vec3( 0.0f, 0.0f, 1.0f ) );
+	SetRotationMatrix(rot);
+}
+
+void CTransform::Pitch( float angle )
+{
+	transform = glm::rotate(transform, glm::radians( angle ), glm::vec3( 1.0f, 0.0f, 0.0f ) );
+}
+void CTransform::Yaw( float angle )
+{
+	transform = glm::rotate(transform, glm::radians( angle ), glm::vec3( 0.0f, 1.0f, 0.0f ) );
+}
+void CTransform::Roll( float angle )
+{
+	transform = glm::rotate(transform, glm::radians( angle ), glm::vec3( 0.0f, 0.0f, 1.0f ) );
+}
+
+
+void CTransform::Rotate( glm::vec3 rotation )
+{
+	glm::quat rotate(rotation);
+	transform = glm::rotate(transform, glm::radians( glm::eulerAngles(rotate).x ), glm::vec3( 1.0f, 0.0f, 0.0f ) );
+	transform = glm::rotate(transform, glm::radians( glm::eulerAngles(rotate).y ), glm::vec3( 0.0f, 1.0f, 0.0f ) );
+	transform = glm::rotate(transform, glm::radians( glm::eulerAngles(rotate).z ), glm::vec3( 0.0f, 0.0f, 1.0f ) );
+	//glm::rotate(const tquat<T, P> &q, const T &angle, const tvec3<T, P> &v);
+	//glm::rotate(const tmat4x4<T, P> &m, T angle, const tvec3<T, P> &v);
+	//glm::rotate(T angle, const tvec3<T, P> &v);
+}
+
+void CTransform::Scale( glm::vec3 scale )
+{
+	glm::scale(transform, scale);
+}
+
+void CTransform::Translate( glm::vec3 translation )
+{
+	glm::translate(transform, translation);
 }
--- src/cTransform.h
+++ src/cTransform.h
@@ -4,47 +4,71 @@
 #include "component.h"
 #include <vector>
 #include "glm/mat4x4.hpp"
-//#include "glm/vec3.hpp"
-//#include "glm/gtc/quaternion.hpp"
+#include "glm/vec3.hpp"
+#include "glm/gtc/quaternion.hpp"
+#include "glm/gtx/transform.hpp"
 
-//TODO: Change components to classes
-//TODO: Add Spaces: Self & World
-struct CTransform : public Component
+
+class CTransform : public Component
 {
     public:
         static const int familyId;
-    
-        CTransform();
+	
+        CTransform( CTransform* parent = nullptr );
         ~CTransform();
-        //std::vector< CTransform* > children;
-        glm::mat4 tranform;
-    
-        //CTransform GetChild( int32_t index );
-        //void LookAt( CTransform target, glm::vec3 worldUp );
-        //void Rotate( glm::vec3 eulerAngles, CTransform relativeTo );
-        //void RotateAround( glm::vec3 point, glm::vec3 axis, float angle );
-        //void SetParent( CTransform parent, bool worldPositionStays = true );
-        //void TransformDirection( glm::vec3 direction );
-        //void TransformPoint( glm::vec3 position );
-        //void TransformVector( glm::vec3 vector );
-        //void Translate( glm::vec3 translation, CTransform relativeTo );
-    
-        //glm::vec3 position;   // position of the transform in world space
-        //glm::vec3 localPosition; // position of the transform relative to the parent transform's.
-        //glm::quat rotation;   // rotation of the transform in world space stored as a Quaternion
-        //glm::quat localRotation; // rotation of the transform relative to the parent transform's rotation
-        //glm::vec3 scale;      // global scale of the object
-        //glm::vec3 localScale; // relative to parent
-        //glm::vec3 eulerAngles;
-        //glm::vec3 localEulerAngles;
-        //glm::vec3 forward;
-        //glm::vec3 right;
-        //glm::vec3 up;
-    
-        //CTransform parent;
-    
+	
+		glm::mat4 transform;
+	
+		std::vector<CTransform* > GetChildren();
+		CTransform* GetParent();
+		void SetParent( CTransform* newParent );
+	
+		void Rotate( glm::vec3 rotate );
+		void Translate( glm::vec3 translation );
+		void Scale( glm::vec3 scale );
+		// POSITION FUNCTIONALITY
+	
+		glm::vec3 GetPosition() const;
+		void SetPosition( glm::vec3 position );
+	
+		glm::vec3 GetLocalPosition() const;
+		void SetLocalPosition( glm::vec3 position );
+
+		// ROTATION FUNCTIONALITY
+		glm::vec3 GetRotation() const;
+		void SetRotation( glm::vec3 rotation );
+	
+		glm::vec3 GetLocalRotation() const;
+		void SetLocalRotation( glm::vec3 rotation );
+	
+		float GetPitch() const;
+		float GetYaw() const;
+		float GetRoll() const;
+	
+		void SetPitch( float angle );
+		void SetYaw( float angle );
+		void SetRoll( float angle );
+	
+		void Pitch( float angle ); // Pitch is the X axis
+		void Yaw( float angle ); // Yaw is the Y axis
+		void Roll( float angle ); // Roll is the Z axis
+	
+		// SCALE FUNCTIONALITY
+	
+		glm::vec3 GetScale() const;
+		void SetScale( glm::vec3 scale );
+	
+		glm::vec3 GetLocalScale() const;
+		void SetLocalScale( glm::vec3 scale );
+	
+	
     private:
-    
+		void SetRotationMatrix( glm::mat4 rot );
+	
+		CTransform* root;
+		CTransform* parent;
+		std::vector<CTransform* > children;
+	
 
 };
 

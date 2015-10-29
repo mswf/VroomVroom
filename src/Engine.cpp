6dd70a4bb09279aacc10177bcc3d7176c9834d2c
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-10-29 17:23:44 +0100

    *Test code & removed event printing

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -235,8 +235,16 @@ void Engine::UpdateLoop()
 		{
 			Update();
             
-            data->model *= scale * rotation * translation;
-            
+			//data->transform.SetPosition( glm::vec3(-15.0f, 0.0f, 2.0f ) );
+			//data->transform.SetRotation( glm::vec3( 45.0f, 45.0f, 0.0f ) );
+			//data->transform.Rotate( glm::vec3( 45.0f, -45.0f, 0.0f ) );
+			
+			//data->transform.Pitch(1.0f);
+			//data->transform.Roll(1.0f);
+			//data->transform.SetPitch(45.0f);
+			//data->transform.SetYaw(45.0f);
+			//data->transform.SetRoll(0.0f);
+			
 			deltaTimeGame -= gameUpdateInterval;
 
 			if (deltaTimeGame < gameUpdateInterval)
@@ -366,69 +374,3 @@ void Engine::GetEntities( std::vector< Entity* > &result )
         result.push_back( iter->second );
     }
 }
-
-void Engine::PrintEvent(const SDL_Event * event)
-{
-    if (event->type == SDL_WINDOWEVENT)
-    {
-        switch (event->window.event)
-        {
-            case SDL_WINDOWEVENT_SHOWN:
-                SDL_Log("Window %d shown", event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_HIDDEN:
-                SDL_Log("Window %d hidden", event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_EXPOSED:
-                SDL_Log("Window %d exposed", event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_MOVED:
-                SDL_Log("Window %d moved to %d,%d",
-                        event->window.windowID, event->window.data1,
-                        event->window.data2);
-                break;
-            case SDL_WINDOWEVENT_RESIZED:
-                SDL_Log("Window %d resized to %dx%d",
-                        event->window.windowID, event->window.data1,
-                        event->window.data2);
-                break;
-            case SDL_WINDOWEVENT_SIZE_CHANGED:
-                SDL_Log("Window %d size changed to %dx%d",
-                        event->window.windowID, event->window.data1,
-                        event->window.data2);
-                break;
-            case SDL_WINDOWEVENT_MINIMIZED:
-                SDL_Log("Window %d minimized", event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_MAXIMIZED:
-                SDL_Log("Window %d maximized", event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_RESTORED:
-                SDL_Log("Window %d restored", event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_ENTER:
-                SDL_Log("Mouse entered window %d",
-                        event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_LEAVE:
-                SDL_Log("Mouse left window %d", event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_FOCUS_GAINED:
-                SDL_Log("Window %d gained keyboard focus",
-                        event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_FOCUS_LOST:
-                SDL_Log("Window %d lost keyboard focus",
-                        event->window.windowID);
-                break;
-            case SDL_WINDOWEVENT_CLOSE:
-                SDL_Log("Window %d closed", event->window.windowID);
-                break;
-            default:
-                SDL_Log("Window %d got unknown event %d",
-                        event->window.windowID, event->window.event);
-                break;
-        }
-    }
-}
-

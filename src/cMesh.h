7f2a0dc4fb802b8f7f2f34c8abff5b6f05676d89
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-03 11:57:42 +0100

    *Update cMesh

--- src/cMesh.h
+++ src/cMesh.h
@@ -1,5 +1,30 @@
 #ifndef cMesh_h
 #define cMesh_h
 
+#include <vector>
+#include "typedef.h"
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
+#include "mesh.h"
+
+class CMesh
+{
+	public:
+	
+		CMesh();
+		~CMesh();
+	
+		uint32 vertexArrayObject;
+		uint32 vertexBufferObject;
+		uint32 indexBufferObject;
+		uint32 numFaces;
+		uint32 vertexLoc;
+		uint32 normalLoc;
+		uint32 texCoordLoc;
+	
+	private:
+	
+	
+};
 
 #endif /* mesh_h */

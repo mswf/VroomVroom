9ddee4d4290580f0cd731d6d10dd573bc0885373
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-06 13:22:35 +0100

    *Clean up component

--- src/Components/component.h
+++ src/Components/component.h
@@ -6,56 +6,16 @@ enum class ComponentTypes
     UNKNOWN = 0,
     TRANSFORM = 1,
     CAMERA,
-    LIGHT,
-    MATERIAL,
-    MESH
+	MESH,
+	MESH_RENDERER,
+    LIGHT
 };
 
+class Entity;
 class Component
-{};
-
-/*
-
-struct CompCamera : public Component
 {
-    // projection matrix =  glm::perspective(glm::radians(fov), aspectRatio, near plane, far plane);
-    //                      glm::ortho(T left, T right, T bottom, T top, near, far);
-    // view matrix = glm::look( Eye vector, Center vector, Up vector);
-    static const FamilyId familyId;
-    CompCamera() {};
+	public:
+		Entity* entity;
 };
-
-struct CompLight : public Component
-{
-    // direction
-    // color
-    // intensity
-    static const FamilyId familyId;
-    CompLight() {};
-};
-
-struct CompMaterial : public Component
-{
-    // shader *
-    // texture *
-    // parameters / uniforms[]
-    static const FamilyId familyId;
-    CompMaterial() {};
-};
-
-struct CompMesh : public Component
-{
-    // VertexBuffer id
-    static const FamilyId familyId;
-    CompMesh() {};
-};
-
-struct CompTexture : public Component
-{
-    // Buffered Texture id
-    static const FamilyId familyId;
-    CompTexture() {};
-};
-*/
 
 #endif /* component_h */

219bcdb91d9a5eb42d1d55dc85ed536fab27368d
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-09 14:57:32 +0100

    *Adding scene graph to entity

--- src/Components/entity.cpp
+++ src/Components/entity.cpp
@@ -1,17 +1,48 @@
-//
-//  entity.cpp
-//  VroomVroom
-//
-//  Created by Valentinas Rimeika on 03/11/15.
-//  Copyright © 2015 Valentinas Rimeika. All rights reserved.
-//
-
 #include "entity.h"
+#include "cTransform.h"
 
 std::multimap< int, Entity* > Entity::componentStorage;
 
-Entity::Entity( std::string name ) :
+Entity::Entity( std::string name, Entity* parent ) :
+	parent(parent),
 	name(name)
 {
-	
+	AddComponent(this, new CTransform());
+}
+
+Entity::~Entity()
+{
+	std::vector< Entity* >::const_iterator iter = children.begin();
+	std::vector< Entity* >::const_iterator end = children.end();
+	for ( ; iter != end; ++iter)
+	{
+		delete *iter;
+	}
+}
+
+void Entity::AddChild( Entity* c )
+{
+	children.push_back(c);
+	c->parent = this;
+}
+
+void Entity::Update()
+{
+	// WorldTransform = (parent != NULL ) ? (parent->WorldTransform * this.Transform) : this.Transform;
+	std::vector< Entity* >::const_iterator iter = children.begin();
+	std::vector< Entity* >::const_iterator end = children.begin();
+	for( ; iter != end; ++iter )
+ 	{
+		(*iter)->Update();
+	}
+}
+
+void Entity::GetChildrenIteratorStart() const
+{
+	//return children.begin();
+}
+
+void Entity::GetChildrenIteratorEnd() const
+{
+	//return children.end();
 }

a50319ba6b693ceccde5ea737e17408430f73161
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-10 15:40:37 +0100

    *Enabling Depth test, Culling

--- src/Systems/renderer.cpp
+++ src/Systems/renderer.cpp
@@ -3,6 +3,7 @@
 #include "../Components/cTransform.h"
 #include "../Components/cCamera.h"
 #include "console.h"
+#include <iostream>
 
 namespace Renderer
 {
@@ -14,8 +15,14 @@ namespace Renderer
 		CMeshRenderer* rend = Entity::GetComponent<CMeshRenderer>(object);
 		Shader* s = rend->material->shader;
 		
+		//glEnable(GL_PROGRAM_POINT_SIZE);
+		glEnable(GL_CULL_FACE);
+		glEnable(GL_BACK);
+		glEnable(GL_DEPTH_TEST);
+		
 		glBindVertexArray( rend->vao );
-		//s->ValidateProgram();
+		
+		s->ValidateProgram();
 		
 		glUseProgram(s->program);
 		
@@ -23,8 +30,8 @@ namespace Renderer
 		s->SetUniformMat4( "view", cam->GetViewMatrix() );
 		s->SetUniformMat4( "projection", cam->GetProjectionMatrix() );
 		s->SetUniformFloat( "time", (float)time );
-
-		glDrawElements( GL_TRIANGLES, rend->numIndices, GL_UNSIGNED_BYTE, 0 );
+		
+		glDrawElements( rend->drawType, rend->numIndices, GL_UNSIGNED_INT, (void*)0 );
 		
 		glBindVertexArray( 0 );
 		glBindBuffer( GL_ARRAY_BUFFER, 0 );

cb50205d095fcd72413668bd8a72bf5b0067cbe3
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-12 12:20:35 +0100

    *Cleaning test code

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,31 @@
 int main(int argc, char** a)
 {
     FMOD::System     *system;
-	FMOD::Sound      *sound1, *sound2, *sound3;
+	FMOD::Sound      *sound1;
 	FMOD::Channel    *channel = 0;
 	FMOD_RESULT       result;
 	unsigned int      version;
 	void             *extradriverdata = 0;
 	
 	result = FMOD::System_Create(&system);
-	
 	result = system->getVersion(&version);
-	
 	if (version < FMOD_VERSION)
 	{
 		std::cout <<  "Error version is not the FMOD version" << std::endl;
 	}
 
 	result = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
+	if (result == FMOD_ERR_INITIALIZATION)
+	{
+		std::cout <<  "Error initializing FMOD" << std::endl;
+	}
 	
 	result = system->createSound("piano2.wav", FMOD_DEFAULT, 0, &sound1);
-	
+	if (result == FMOD_ERR_FILE_NOTFOUND)
+	{
+		std::cout <<  "Error: FMOD did not find the file" << std::endl;
+	}
 	result = sound1->setMode(FMOD_LOOP_OFF);
-	
 	result = system->playSound(sound1, 0, false, &channel);
 	
 	Engine engine;

ba196e78fbd735506386c8d2328fbf381c88b4f9
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-12 11:56:28 +0100

    ****** TESTING FMOD CODE *******

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,36 @@
 #include "Tests/TestCode.h"
 #endif
 
+#include "fmod.hpp"
+
+#include <iostream>
+
 int main(int argc, char** a)
 {
-    
+    FMOD::System     *system;
+	FMOD::Sound      *sound1, *sound2, *sound3;
+	FMOD::Channel    *channel = 0;
+	FMOD_RESULT       result;
+	unsigned int      version;
+	void             *extradriverdata = 0;
+	
+	result = FMOD::System_Create(&system);
+	
+	result = system->getVersion(&version);
+	
+	if (version < FMOD_VERSION)
+	{
+		std::cout <<  "Error version is not the FMOD version" << std::endl;
+	}
+
+	result = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
+	
+	result = system->createSound("piano2.wav", FMOD_DEFAULT, 0, &sound1);
+	
+	result = sound1->setMode(FMOD_LOOP_OFF);
+	
+	result = system->playSound(sound1, 0, false, &channel);
+	
 	Engine engine;
 	engine.InitSDL();
 	engine.InitSDLNet();

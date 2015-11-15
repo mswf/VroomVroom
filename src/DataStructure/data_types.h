4430b062b08c23534b1b78503641e1bdea2625a8
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-15 13:44:40 +0100

    *Add bitangent & tangent to mesh

--- src/DataStructure/data_types.h
+++ src/DataStructure/data_types.h
@@ -10,9 +10,12 @@ struct Mesh
 	std::vector< glm::vec3 > vertices;
 	std::vector< glm::vec2 > uvs;
 	std::vector< glm::vec3 > normals;
+	std::vector< glm::vec3 > bitangents;
+	std::vector< glm::vec3 > tangents;
 	std::vector< unsigned int > indices;
 	unsigned int numIndices;
 	bool hasNormals;
+	bool hasTangentsAndBitangets;
 	bool hasUVs;
 };
 

8aad24bd3a88a9f3e053473b048175c2fd9d518b
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-17 00:03:06 +0100

    *Texture buffering, deleting, mipmap generation

--- src/DataStructure/texture.cpp
+++ src/DataStructure/texture.cpp
@@ -1,1 +1,42 @@
 #include "Texture.h"
+
+GLuint BufferTexture2D( GLenum target, GLint levelOfDetail, GLint internalFormat, GLint width, GLint height, GLint pixelFormat, GLenum type, GLvoid* data, const std::vector< std::pair< GLenum, GLint > >* textureParameters, bool generateMipMap )
+{
+	unsigned int textureId;
+	glGenTextures( 1, &textureId );
+	glBindTexture( target, textureId );
+	
+	std::vector< std::pair< GLenum, GLint > >::const_iterator iter = textureParameters->begin();
+	std::vector< std::pair< GLenum, GLint > >::const_iterator end = textureParameters->end();
+	for ( ; iter != end; ++iter )
+	{
+		glTexParameteri( target, (*iter).first, (*iter).second );
+	}
+	glTexImage2D( target, levelOfDetail, internalFormat, width, height, 0, pixelFormat, type, data );
+	if ( generateMipMap ) glGenerateMipmap( GL_TEXTURE_2D );
+	
+	return textureId;
+}
+
+GLuint BufferTexture1D( GLenum target, GLint levelOfDetail, GLint internalFormat, GLint width, GLint pixelFormat, GLenum type, GLvoid* data, const std::vector< std::pair< GLenum, GLint > >* textureParameters, bool generateMipMap )
+{
+	unsigned int textureId;
+	glGenTextures( 1, &textureId );
+	glBindTexture( target, textureId );
+	
+	std::vector< std::pair< GLenum, GLint > >::const_iterator iter = textureParameters->begin();
+	std::vector< std::pair< GLenum, GLint > >::const_iterator end = textureParameters->end();
+	for ( ; iter != end; ++iter )
+	{
+		glTexParameteri( target, (*iter).first, (*iter).second );
+	}
+	glTexImage1D( target, levelOfDetail, internalFormat, width, 0, pixelFormat, type, data );
+	if ( generateMipMap ) glGenerateMipmap( GL_TEXTURE_1D );
+	
+	return textureId;
+}
+
+void DeleteTexture( GLuint textureId )
+{
+	glDeleteTextures(1, &textureId);
+}
--- src/DataStructure/texture.h
+++ src/DataStructure/texture.h
@@ -1,5 +1,27 @@
 #ifndef Texture_h
 #define Texture_h
 
+#include "glew.h"
+#include <map>
+#include <vector>
+
+// TODO(Valentinas): Add possibility to allocate or deallocate multiple textures in one go
+// TODO(Valentinas): Support odd texture dimensions
+
+GLuint BufferTexture2D( GLenum target, GLint levelOfDetail, GLint internalFormat, GLint width, GLint height, GLint pixelFormat, GLenum type, GLvoid* data, const std::vector< std::pair< GLenum, GLint > >* textureParameters, bool generateMipMap = false );
+
+GLuint BufferTexture1D( GLenum target, GLint levelOfDetail, GLint internalFormat, GLint width, GLint pixelFormat, GLenum type, GLvoid* data, const std::vector< std::pair< GLenum, GLint > >* textureParameters, bool generateMipMap = false );
+
+void DeleteTexture( GLuint textureId );
 
 #endif /* Texture_h */
+
+/*
+
+	// we also want to be able to deal with odd texture dimensions
+ 	glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
+	glPixelStorei( GL_UNPACK_ROW_LENGTH, 0 );
+	glPixelStorei( GL_UNPACK_SKIP_PIXELS, 0 );
+	glPixelStorei( GL_UNPACK_SKIP_ROWS, 0 );
+ 
+*/

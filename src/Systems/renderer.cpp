36afee787c070c897fb061d267350c45447c4dab
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-23 11:22:20 +0100

    *Small changes

--- src/Systems/renderer.cpp
+++ src/Systems/renderer.cpp
@@ -3,6 +3,7 @@
 #include "../Components/cTransform.h"
 #include "../Components/cCamera.h"
 #include "console.h"
+#include "texture.h"
 #include <iostream>
 
 namespace Renderer
@@ -12,47 +13,48 @@ namespace Renderer
 	{
 		CTransform* trans = Entity::GetComponent<CTransform>(object);
 		CCamera* cam =		Entity::GetComponent<CCamera>(camera);
-		CMeshRenderer* rend = Entity::GetComponent<CMeshRenderer>(object);
-		Shader* s = rend->material->shader;
+		ModelInstance* m = Entity::GetComponent<CMeshRenderer>(object)->model;
+		Material* mtl = Entity::GetComponent<CMeshRenderer>(object)->material;
+		Shader* s = mtl->shader;
+		
+		if (m == NULL) return;
 		
 		//glEnable(GL_PROGRAM_POINT_SIZE);
 		glEnable(GL_CULL_FACE);
 		glEnable(GL_BACK);
+		//glPolygonMode(GL_FRONT, m->material->wireframe_enabled ? GL_FILL : GL_LINE );
 		glEnable(GL_DEPTH_TEST);
 		
-		glBindVertexArray( rend->vao );
-		//s->ValidateProgram();
+		glBindVertexArray( m->vao );
 		
-		glUseProgram(s->program);
+		mtl->UseMaterial();
 		
 		glm::mat3 mvMatrix = glm::mat3( cam->GetViewMatrix()* trans->GetTransform() );
 		glm::mat3 normalMatrix = glm::transpose(glm::inverse(mvMatrix));
 		glm::vec3 lightPosition( 1.0 );
 		
-		s->SetUniformMat4( "model", trans->GetTransform() );
-		s->SetUniformMat4( "view", cam->GetViewMatrix() );
-		s->SetUniformMat3( "normalMatrix", normalMatrix );
-		s->SetUniformMat3( "mvMatrix", mvMatrix );
-		s->SetUniformMat4( "projection", cam->GetProjectionMatrix() );
-		s->SetUniformFloat( "time", (float)time );
-		s->SetUniformFloat3( "lightPos", lightPosition);
+		s->SetUniformMat4( 		"model", 		trans->GetTransform() );
+		s->SetUniformMat4( 		"view", 		cam->GetViewMatrix() );
+		s->SetUniformMat3( 		"normalMatrix", normalMatrix );
+		s->SetUniformMat3( 		"mvMatrix", 	mvMatrix );
+		s->SetUniformMat4( 		"projection", 	cam->GetProjectionMatrix() );
+		s->SetUniformFloat( 	"time", 		(float)time );
+		s->SetUniformFloat3( 	"lightPos", 	lightPosition);
 		
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture( GL_TEXTURE_2D, rend->material->diff_texture );
+		BindTexture( GL_TEXTURE0, GL_TEXTURE_2D, mtl->diffuseTextureId);
 		s->SetUniformInt( "colorMap", 0 );
 		
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture( GL_TEXTURE_2D, rend->material->normal_texture );
+		BindTexture( GL_TEXTURE1, GL_TEXTURE_2D, mtl->normalTextureId);
 		s->SetUniformInt( "normalMap", 1 );
 		
-		glDrawElements( rend->drawType, rend->numIndices, GL_UNSIGNED_INT, (void*)0 );
+		glDrawElements( GL_TRIANGLES, m->numIndices, GL_UNSIGNED_INT, (void*)0 );
 		
 		glBindVertexArray( 0 );
 		glBindBuffer( GL_ARRAY_BUFFER, 0 );
 		
 		//TODO(Valentinas): Disable Vertex Attributes after drawing
 		
-		//glBindTexture(GL_ACTIVE_TEXTURE, 0);
+		UnbindTexture();
 		glUseProgram(0);
 	}
 	

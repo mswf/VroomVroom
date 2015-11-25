245fd460d9be280162a674fb8d14acdc9cbc7d22
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-25 15:00:06 +0100

    *Change from multiple buffers to one vbo using BufferSubData

--- src/DataStructure/mesh.cpp
+++ src/DataStructure/mesh.cpp
@@ -21,13 +21,16 @@ void BufferMesh( const Mesh* m, ModelInstance* instance, Material* mtl )
 	
 	//VBO, allocate data and upload data from CPU to GPU
 	
+	/*
 	GLint position_attrib_index = 	glGetAttribLocation(p, "position");
 	GLint normal_attrib_index = 	glGetAttribLocation(p, "normal");
 	GLint texcoord_attrib_index = 	glGetAttribLocation(p, "texcoord");
 	GLint tangent_attrib_index = 	glGetAttribLocation(p, "tangent");
 	GLint bitangent_attrib_index = 	glGetAttribLocation(p, "bitangent");
-	
+	*/
 	//Terminal.LogRender( "Buffering vertices" );
+	
+	/*
 	
 	glGenBuffers( 1, &instance->vertexBuffer );
 	glBindBuffer( GL_ARRAY_BUFFER, instance->vertexBuffer );
@@ -35,39 +38,67 @@ void BufferMesh( const Mesh* m, ModelInstance* instance, Material* mtl )
 	glEnableVertexAttribArray( position_attrib_index );
 	glVertexAttribPointer( position_attrib_index, 3, GL_FLOAT, GL_FALSE, 0, 0 );
 	
+	*/
+
+	glGenBuffers( 1, &instance->vbo );
+	glBindBuffer( GL_ARRAY_BUFFER, instance->vbo );
+	unsigned long v, n, u, t, bt, buf, v3, v2;
+	
+	v3 = sizeof(glm::vec3);
+	v2 = sizeof(glm::vec2);
+	v = m->vertices.size() * v3;
+	n = m->normals.size() * v3;
+	u = m->uvs.size() * v2;
+	t = m->tangents.size() * v3;
+	bt = m->bitangents.size()* v3;
+	buf = v + n + u + t + bt;
+	
+	glBufferData( GL_ARRAY_BUFFER, buf, NULL, GL_STATIC_DRAW);
+	
+	// Buffer vertices
+	//glBufferData( GL_ARRAY_BUFFER, m->vertices.size() * sizeof(glm::vec3), &m->vertices.front(), GL_STATIC_DRAW);
+	glBufferSubData( GL_ARRAY_BUFFER, 0, m->vertices.size() * sizeof(glm::vec3), &m->vertices.front() );
+	glEnableVertexAttribArray( 0 ); //position_attrib_index
+	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, 0 );
+	
 	if (m->hasNormals)
 	{
 		//Terminal.LogRender( "Buffering normals" );
-		glGenBuffers( 1, &instance->normalBuffer );
-		glBindBuffer( GL_ARRAY_BUFFER, instance->normalBuffer );
-		glBufferData( GL_ARRAY_BUFFER, m->normals.size() * sizeof(glm::vec3), &m->normals.front(), GL_STATIC_DRAW);
-		glEnableVertexAttribArray( normal_attrib_index );
-		glVertexAttribPointer( normal_attrib_index, 3, GL_FLOAT, GL_FALSE, 0, 0 );
+		//glGenBuffers( 1, &instance->normalBuffer );
+		//glBindBuffer( GL_ARRAY_BUFFER, instance->normalBuffer );
+		//glBufferData( GL_ARRAY_BUFFER, m->normals.size() * sizeof(glm::vec3), &m->normals.front(), GL_STATIC_DRAW);
+		glBufferSubData( GL_ARRAY_BUFFER, v, m->normals.size() * sizeof(glm::vec3), &m->normals.front() );
+		glEnableVertexAttribArray( 1 ); // normal_attrib_index
+		//glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, 0 );
+		glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, (void*)v );
 	}
 	
 	if (m->hasUVs)
 	{
 		//Terminal.LogRender( "Buffering uvs" );
-		glGenBuffers( 1, &instance->uvBuffer );
-		glBindBuffer( GL_ARRAY_BUFFER, instance->uvBuffer );
-		glBufferData( GL_ARRAY_BUFFER, m->uvs.size() * sizeof(glm::vec2), &m->uvs.front(), GL_STATIC_DRAW);
-		glVertexAttribPointer( texcoord_attrib_index, 2, GL_FLOAT, GL_FALSE, 0, 0 );
-		glEnableVertexAttribArray( texcoord_attrib_index );
+		//glGenBuffers( 1, &instance->uvBuffer );
+		//glBindBuffer( GL_ARRAY_BUFFER, instance->uvBuffer );
+		//glBufferData( GL_ARRAY_BUFFER, m->uvs.size() * sizeof(glm::vec2), &m->uvs.front(), GL_STATIC_DRAW);
+		glBufferSubData( GL_ARRAY_BUFFER, v+n, m->uvs.size() * sizeof(glm::vec2), &m->uvs.front() );
+		glEnableVertexAttribArray( 2 ); // texcoord_attrib_index
+		glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, 0, (void*)(v+n) );
 	}
 	
 	if (m->hasTangentsAndBitangets)
 	{
-		glGenBuffers( 1, &instance->tangentBuffer );
-		glBindBuffer( GL_ARRAY_BUFFER, instance->tangentBuffer );
-		glBufferData( GL_ARRAY_BUFFER, m->tangents.size() * sizeof(glm::vec3), &m->tangents.front(), GL_STATIC_DRAW);
-		glEnableVertexAttribArray( tangent_attrib_index );
-		glVertexAttribPointer( tangent_attrib_index, 3, GL_FLOAT, GL_FALSE, 0, 0 );
+		//glGenBuffers( 1, &instance->tangentBuffer );
+		//glBindBuffer( GL_ARRAY_BUFFER, instance->tangentBuffer );
+		//glBufferData( GL_ARRAY_BUFFER, m->tangents.size() * sizeof(glm::vec3), &m->tangents.front(), GL_STATIC_DRAW);
+		glBufferSubData( GL_ARRAY_BUFFER, v+n+u, m->tangents.size() * sizeof(glm::vec3), &m->tangents.front() );
+		glEnableVertexAttribArray( 3 ); // tangent_attrib_index
+		glVertexAttribPointer( 3, 3, GL_FLOAT, GL_FALSE, 0, (void*)(v+n+u) );
 		
-		glGenBuffers( 1, &instance->bitangentBuffer );
-		glBindBuffer( GL_ARRAY_BUFFER, instance->bitangentBuffer );
-		glBufferData( GL_ARRAY_BUFFER, m->bitangents.size() * sizeof(glm::vec3), &m->bitangents.front(), GL_STATIC_DRAW);
-		glEnableVertexAttribArray( bitangent_attrib_index );
-		glVertexAttribPointer( bitangent_attrib_index, 3, GL_FLOAT, GL_FALSE, 0, 0 );
+		//glGenBuffers( 1, &instance->bitangentBuffer );
+		//glBindBuffer( GL_ARRAY_BUFFER, instance->bitangentBuffer );
+		//glBufferData( GL_ARRAY_BUFFER, m->bitangents.size() * sizeof(glm::vec3), &m->bitangents.front(), GL_STATIC_DRAW);
+		glBufferSubData( GL_ARRAY_BUFFER, v+n+u+t, m->bitangents.size() * sizeof(glm::vec3), &m->bitangents.front() );
+		glEnableVertexAttribArray( 4 ); // bitangent_attrib_index
+		glVertexAttribPointer( 4, 3, GL_FLOAT, GL_FALSE, 0, (void*)(v+n+u+t) );
 	}
 	
 	/*
@@ -94,9 +125,10 @@ void BufferMesh( const Mesh* m, ModelInstance* instance, Material* mtl )
 void UnBuffer( ModelInstance* instance )
 {
 	glDeleteVertexArrays( 1, &instance->vao );
-	glDeleteBuffers( GL_ARRAY_BUFFER, &instance->vertexBuffer );
-	glDeleteBuffers( GL_ARRAY_BUFFER, &instance->normalBuffer );
-	glDeleteBuffers( GL_ARRAY_BUFFER, &instance->uvBuffer );
+	glDeleteBuffers( GL_ARRAY_BUFFER, &instance->vbo );
+	//glDeleteBuffers( GL_ARRAY_BUFFER, &instance->vertexBuffer );
+	//glDeleteBuffers( GL_ARRAY_BUFFER, &instance->normalBuffer );
+	//glDeleteBuffers( GL_ARRAY_BUFFER, &instance->uvBuffer );
 	glDeleteBuffers( GL_ELEMENT_ARRAY_BUFFER, &instance->indiceBuffer );
 	
 }

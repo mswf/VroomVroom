d39cd1c8f28d5da789b9c9752a2b7e314a5dab0d
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-29 20:24:29 +0100

    +Add opengl_common to Utilities

---
+++ src/Utilities/opengl_common.cpp
@@ -0,0 +1,9 @@
+//
+//  opengl_common.cpp
+//  VroomVroom
+//
+//  Created by Valentinas Rimeika on 29/11/15.
+//  Copyright © 2015 Valentinas Rimeika. All rights reserved.
+//
+
+#include "opengl_common.hpp"
---
+++ src/Utilities/opengl_common.h
@@ -0,0 +1,14 @@
+//
+//  opengl_common.hpp
+//  VroomVroom
+//
+//  Created by Valentinas Rimeika on 29/11/15.
+//  Copyright © 2015 Valentinas Rimeika. All rights reserved.
+//
+
+#ifndef opengl_common_hpp
+#define opengl_common_hpp
+
+#include <stdio.h>
+
+#endif /* opengl_common_hpp */

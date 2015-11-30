9be31215e131c851f24292f10f4436852d8849f6
Valentinas Rimeika <valentinasr8901@gmail.com>
2015-11-30 23:41:29 +0100

    *Change shader from a class to a pile of functions

--- src/DataStructure/shader.h
+++ src/DataStructure/shader.h
@@ -12,98 +12,84 @@
 
 enum class GLSLShaderType
 {
+	NONE,
 	VERTEX,
 	GEOMETRY,
 	FRAGMENT,
 	TESS_CONTROL,
 	TESS_EVALUATION
 };
+	
+void UseProgram( GLuint program );
+void UseProgramStages( GLuint pPipeline, GLSLShaderType shaderType, GLuint program );
+void CreateShader( GLuint& shader, GLenum shaderType, const char* source );
 
-class Shader
-{
-	public:
-	
-		GLuint program;
-	
-		Shader();
-		Shader( std::string vertex, std::string fragment );
-	
-		~Shader();
-		void UseProgram();
-		void UseProgramStages( GLuint pPipeline, GLSLShaderType shaderType, GLuint program );
-		GLuint CreateShader( GLSLShaderType shaderType, const char* source );
-	
-		template<typename... Targs>
-		GLuint CreateProgram( Targs... shaders );
-		GLuint CreateSeparateProgram( GLenum shaderObject );
-	
-		void DeleteProgram( GLuint program );
-		void DeleteShaderObject( GLuint shader );
-		void DetachShader( GLuint program, GLuint shader );
-		void AttachShader( GLuint program, GLuint shader );
-		void LinkProgram( GLuint program );
-		bool ValidateProgram( GLuint shaderProgram );
-		bool IsShader( GLuint shader );
-		bool IsProgram( GLuint program );
-	
-		void SetUniform( const char* name, int val );
-		void SetUniform( const char* name, unsigned int val );
-		void SetUniform( const char* name, float val );
-		void SetUniform( const char* name, double val );
-		void SetUniform( const char* name, const glm::vec2& vec );
-		void SetUniform( const char* name, const glm::vec3& vec );
-		void SetUniform( const char* name, const glm::vec4& vec );
-		void SetUniform( const char* name, const glm::ivec2& vec );
-		void SetUniform( const char* name, const glm::ivec3& vec );
-		void SetUniform( const char* name, const glm::ivec4& vec );
-		void SetUniform( const char* name, const glm::uvec2& vec );
-		void SetUniform( const char* name, const glm::uvec3& vec );
-		void SetUniform( const char* name, const glm::uvec4& vec );
-		void SetUniform( const char* name, const glm::mat2& mat );
-		void SetUniform( const char* name, const glm::mat3& mat );
-		void SetUniform( const char* name, const glm::mat4& mat );
-	
-		void SetActiveSubroutine( GLuint program, ShaderObject shader, const char* uniform, const char* routine );
-	
-		void LogActiveAttributes( GLuint program );
-		void LogActiveUniforms( GLuint program );
-		void LogActiveSubroutines( GLuint shaderProgram, GLenum shaderType );
-	
-	
-private:
-	
-		unsigned int GetSubroutineUniformLocation( const char* name, const std::map< std::string, unsigned int >& locations );
-		void SetSubroutineUniformLocations( GLuint program, ShaderObject& shader );
-	
-		GLenum GetGLShaderEnum( GLSLShaderType type, bool separate = false );
-		bool ProgramInfoLog( GLuint program, GLenum status );
-		bool ShaderInfoLog( GLuint shader, GLenum status );
-	
-		const char* builtin_vertex =
-		"#version 410\n"
-		"layout(location = 0) in vec3 position;\n"
-		"layout(location = 1) in vec2 texcoord;\n"
-		"uniform mat4 model;\n"
-		"uniform mat4 view;\n"
-		"uniform mat4 projection;\n"
-		"out vec2 uv;\n"
-		"void main()\n"
-		"{\n"
-		"	uv = texcoord;\n"
-		"	gl_Position = projection * view * model * vec4( position, 1.0 );\n"
-		"}\n";
-	
-		const char* builtin_fragment =
-		"#version 410\n"
-		"uniform sampler2D colorMap;\n"
-		"uniform float time;\n"
-		"in vec2 uv;\n"
-		"out vec4 Out_Color;\n"
-		"void main()\n"
-		"{\n"
-		"	Out_Color = vec4(1.0, 1.0, 0.0, 1.0) * texture( colorMap, vec2(uv.s, uv.t * time) ); \n"
-		"}\n";
-};
+void CreateProgram( GLuint& program, const unsigned int* shaders, unsigned int count, bool separate = false );
+
+void DeleteProgram( GLuint program );
+void DeleteShaderObject( GLuint shader );
+void DetachShader( GLuint program, GLuint shader );
+void AttachShader( GLuint program, GLuint shader );
+void LinkProgram( GLuint program );
+bool ValidateProgram( GLuint program );
+bool IsShader( GLuint shader );
+bool IsProgram( GLuint program );
+
+void SetUniform( GLuint program, const char* name, int val );
+void SetUniform( GLuint program, const char* name, unsigned int val );
+void SetUniform( GLuint program, const char* name, float val );
+void SetUniform( GLuint program, const char* name, double val );
+void SetUniform( GLuint program, const char* name, const glm::vec2& vec );
+void SetUniform( GLuint program, const char* name, const glm::vec3& vec );
+void SetUniform( GLuint program, const char* name, const glm::vec4& vec );
+void SetUniform( GLuint program, const char* name, const glm::ivec2& vec );
+void SetUniform( GLuint program, const char* name, const glm::ivec3& vec );
+void SetUniform( GLuint program, const char* name, const glm::ivec4& vec );
+void SetUniform( GLuint program, const char* name, const glm::uvec2& vec );
+void SetUniform( GLuint program, const char* name, const glm::uvec3& vec );
+void SetUniform( GLuint program, const char* name, const glm::uvec4& vec );
+void SetUniform( GLuint program, const char* name, const glm::mat2& mat );
+void SetUniform( GLuint program, const char* name, const glm::mat3& mat );
+void SetUniform( GLuint program, const char* name, const glm::mat4& mat );
+
+bool HasSubroutines( const GLuint& program, GLenum shaderType );
+void SetActiveSubroutine( GLuint program, ShaderObject shader, const char* uniform, const char* routine );
+
+void LogActiveAttributes( GLuint program );
+void LogActiveUniforms( GLuint program );
+void LogActiveSubroutines( GLuint program, GLenum shaderType );
+
+unsigned int GetSubroutineUniformLocation( const char* name, const std::map< std::string, unsigned int >& locations );
+void SetSubroutineUniformLocations( GLuint program, ShaderObject& shader );
+
+GLenum GetGLShaderEnum( GLSLShaderType type, bool separate = false );
+bool ProgramInfoLog( GLuint program, GLenum status );
+bool ShaderInfoLog( GLuint shader, GLenum status );
+
+static const char* builtin_vertex =
+"#version 410\n"
+"layout(location = 0) in vec3 position;\n"
+"layout(location = 2) in vec2 texcoord;\n"
+"uniform mat4 model;\n"
+"uniform mat4 view;\n"
+"uniform mat4 projection;\n"
+"out vec2 uv;\n"
+"void main()\n"
+"{\n"
+"	uv = texcoord;\n"
+"	gl_Position = projection * view * model * vec4( position, 1.0 );\n"
+"}\n";
+
+static const char* builtin_fragment =
+"#version 410\n"
+"uniform sampler2D colorMap;\n"
+"uniform float time;\n"
+"in vec2 uv;\n"
+"out vec4 Out_Color;\n"
+"void main()\n"
+"{\n"
+"	Out_Color = texture( colorMap, uv ); \n"
+"}\n";
 
 
 #endif /* shader_h */

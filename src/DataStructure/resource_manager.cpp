4ace65368de9e824a71ffe859498e8b4c5bc93fa
Steff Kempink <mswf@live.nl>
2015-12-04 15:59:57 +0100

    * changed default texture values; always mipmap

--- src/DataStructure/resource_manager.cpp
+++ src/DataStructure/resource_manager.cpp
@@ -260,11 +260,11 @@ bool ResourceManager::BufferImage2D( const char* name )
 	if ( !img->isBuffered || iter_imageId == imageIds.end() )
 	{
 		std::vector< std::pair<GLenum, GLint> >* textureParameters = new std::vector< std::pair<GLenum, GLint> >();
-		std::pair< GLenum, GLint > param( GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-		std::pair< GLenum, GLint > param2( GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+		std::pair< GLenum, GLint > param( GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
+		std::pair< GLenum, GLint > param2(GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 		textureParameters->push_back( param );
 		textureParameters->push_back( param2 );
-		img->imageId = BufferTexture2D( 0, GL_RGBA, img->width, img->height, GL_RGBA, GL_UNSIGNED_BYTE, img->pixelData, textureParameters );
+		img->imageId = BufferTexture2D( 0, GL_RGBA, img->width, img->height, GL_RGBA, GL_UNSIGNED_BYTE, img->pixelData, textureParameters, true);
 		img->isBuffered = true;
 		imageIds.insert( std::pair< std::string, unsigned int >( std::string(name), img->imageId ) );
 	}
